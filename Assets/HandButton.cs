41aaf00a57031e8f306c2f7ff123cb12c3ba8a93
dav1nn <davinayyubov@gmail.com>
2023-12-20 01:43:58 +0000

    made fire particle appear when button press
    forgot to mention i made fire particle in previous push

--- Assets/HandButton.cs
+++ Assets/HandButton.cs
@@ -1,8 +1,28 @@
 using UnityEngine;
-using UnityEngine.Events;
+using System.Collections.Generic; // Import for using List
 using UnityEngine.XR.Interaction.Toolkit;
 
 public class HandButton : XRBaseInteractable
 {
+    public List<ParticleSystem> controlledParticleSystems; // List of particle systems
 
+    protected override void OnSelectEntered(SelectEnterEventArgs args)
+    {
+        base.OnSelectEntered(args);
+        ToggleParticleSystems();
+    }
+
+    private void ToggleParticleSystems()
+    {
+        foreach (var ps in controlledParticleSystems)
+        {
+            if (ps != null)
+            {
+                if (ps.isPlaying)
+                    ps.Stop();
+                else
+                    ps.Play();
+            }
+        }
+    }
 }
